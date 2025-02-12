d2a754276f1135671a4741bf9881e30c3564a31e
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-02-12 23:50:12 +0000

    Updating to latest FlutterFlow output.

--- lib/login/login_widget.dart
+++ lib/login/login_widget.dart
@@ -792,7 +792,7 @@ class _LoginWidgetState extends State<LoginWidget>
                                   16.0, 0.0, 16.0, 0.0),
                               iconPadding: EdgeInsetsDirectional.fromSTEB(
                                   0.0, 0.0, 0.0, 0.0),
-                              color: FlutterFlowTheme.of(context).primary,
+                              color: FlutterFlowTheme.of(context).tertiary,
                               textStyle: FlutterFlowTheme.of(context)
                                   .labelMedium
                                   .override(
